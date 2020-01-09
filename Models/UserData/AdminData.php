863619480b742131be60bc609e8602cdf91125a6
dcooper13 <59364744+dcooper13@users.noreply.github.com>
2020-01-09 16:35:01 +0000

    Rubric System: Login

---
+++ Models/UserData/AdminData.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class AdminData extends UserData
+{
+    public function viewUsers()
+    {
+        return ;
+    }
+    public function addUser()
+    {
+        return ;
+    }
+    public function deleteUser()
+    {
+        return ;
+    }
+    public function setUserRole()
+    {
+        return ;
+    }
+    public function setUserClass()
+    {
+        return ;
+    }
+    public function createRubric()
+    {
+        return ;
+    }
+    public function editRubric()
+    {
+        return ;
+    }
+    public function deleteRubric()
+    {
+        return ;
+    }
+}
