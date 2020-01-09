5eacd36b509c0093d162c2ed4cd5adde16ffb89a
kirky1 <alex.kirkman53@gmail.com>
2020-01-09 16:24:25 +0000

    Alex V2

--- Models/UserData/UserData.php
+++ Models/UserData/UserData.php
@@ -3,27 +3,77 @@
 
 class UserData
 {
-    protected $_user_id, $_username, $_email;
+    protected $userID, $roleID, $firstName, $lastName, $username, $email, $password;
 
-    public function __construct($dbRow)
+    public function __construct($user_data)
     {
-        $this->_user_id = $dbRow['user_id'];
-        $this->_username = $dbRow['username'];
-        $this->_email= $dbRow['email'];
+        if () {
+
+        }
+
+        $this->userID = $user_data['userID'];
+        $this->roleID = $user_data['roleID'];
+        $this->firstName = $user_data['firstName'];
+        $this->lastName = $user_data['lastName'];
+        $this->username = $user_data['userName'];
+        $this->email = $user_data['email'];
+        $this->password = $user_data['password'];
+
+    }
+
+    /**
+     * Logs the user in
+     *
+     */
+    public function login(){
+
     }
 
     public function getUserID()
     {
-        return $this->_user_id;
+        return $this->userID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoleID()
+    {
+        return $this->roleID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
     }
 
     public function getUsername()
     {
-        return $this->_username;
+        return $this->username;
     }
 
     public function getEmail()
     {
-        return $this->_email;
+        return $this->email;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
 }
