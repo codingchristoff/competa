05296705c73c30682399d874fa3558c45ece9513
skysnip3z <jcu8710@gmail.com>
2020-01-09 18:08:30 +0000

    ali test

--- Models/UserData/AdminData.php
+++ Models/UserData/AdminData.php
@@ -3,34 +3,46 @@
 
 class AdminData extends UserData
 {
+    public function __construct($user_data)
+    {
+        parent::__construct($user_data);
+    }
+
     public function viewUsers()
     {
         return ;
     }
+
     public function addUser()
     {
         return ;
     }
+
     public function deleteUser()
     {
         return ;
     }
+
     public function setUserRole()
     {
         return ;
     }
+
     public function setUserClass()
     {
         return ;
     }
+
     public function createRubric()
     {
         return ;
     }
+
     public function editRubric()
     {
         return ;
     }
+
     public function deleteRubric()
     {
         return ;
---
+++ Models/UserData/ClassData.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("Models/UserDataSet.php");
+
+class ClassData
+{
+    protected $_clasID
+
+    public function __construct()
+    {
+
+    }
+}
