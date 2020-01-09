fa66925ac625677dcb5e51e5e97ea42c04aa44d2
skysnip3z <jcu8710@gmail.com>
2020-01-09 18:49:47 +0000

    ali end of day 09/01/20

--- Models/UserData/ClassData.php
+++ Models/UserData/ClassData.php
@@ -2,11 +2,34 @@
 
 require_once("Models/UserDataSet.php");
 
-class ClassData
+class ClassData extends UserData
 {
-    protected $_clasID
+    protected $_classID, $_user_ds;
 
-    public function __construct()
+    public function __construct($user_data)
+    {
+        parent::__construct($user_data);
+        $this->_classID = $user_data['classID'];
+        $this->_user_ds = new UserDataSet();
+    }
+
+    public function getInfo($userName)
+    {
+        $user = $this->_user_ds->fetchUser($userName);
+
+        if($user instanceof AdminData || $user instanceof TeacherData)
+        {}
+        else {
+            return $user;
+        }
+    }
+
+    public function setRole()
+    {
+
+    }
+
+    public function getUserID()
     {
 
     }
