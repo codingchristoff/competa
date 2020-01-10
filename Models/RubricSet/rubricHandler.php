af35c30e47e9aff89de7e4d10f704d2cc36ba05e
codingchristoff <50449421+codingchristoff@users.noreply.github.com>
2020-01-10 17:50:05 +0000

    added new methods into rubric class for testing

--- Models/RubricSet/rubricHandler.php
+++ Models/RubricSet/rubricHandler.php
@@ -1,7 +1,34 @@
 <?php
 
+require_once('Models/Database.php');
+require_once('Models/RubricSet/Rubric.php');
+require_once('Models/RubricSet/Categories.php');
+require_once('Models/RubricSet/Criteria.php');
 
 class rubricHandler
 {
+    protected $_dbHandle, $_dbInstance;
+
+    public function __construct()
+    {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getdbConnection();
+    }
     
+    public function test($rubericName)
+    {
+        $sqlQuery = 'SELECT * FROM rubrics WHERE rubricName = :rubricName';
+    
+        $stmt = $this->_dbHandle->prepare($sqlQuery); // prepare PDO statement
+
+        $stmt->bindParam(":rubricName", $param_rubericName, PDO::PARAM_STR);
+
+        // Set parameters
+        $param_rubericName = $rubericName;
+
+        $stmt->execute(); // execute the PDO statement
+
+        return new Rubric($stmt->fetch());  // return an array of UserData objects
+    }
+
 }
