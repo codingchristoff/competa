733904a486746f5024fe2bdd650a9577a1b0d06d
kirky1 <alex.kirkman53@gmail.com>
2020-01-10 12:38:15 +0000

    Alex - Final version UserData

--- Models/UserData/ClassData.php
+++ Models/UserData/ClassData.php
@@ -6,11 +6,7 @@ class ClassData extends UserData
 {
     protected $_classID, $_user_ds;
 
-<<<<<<< HEAD
-    public function __construct()
-=======
     public function __construct($user_data)
->>>>>>> d1e094f687aa587e22f106520806ddb48413921f
     {
         parent::__construct($user_data);
         $this->_classID = $user_data['classID'];
