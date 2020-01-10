27b8886fd348210622962421b4b813bc96359f97
dcooper13 <59364744+dcooper13@users.noreply.github.com>
2020-01-10 12:37:20 +0000

    Duane TAble and Group class template V1

---
+++ Models/UserData/Group.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Group
+{
+    /**
+     * Group constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     *  Requires DataSet to implement
+     */
+    public function a()
+    {
+        return ;
+    }
+
+}
