d1048adf07aa50365f017b87f0706bed0a7f58b8
skysnip3z <jcu8710@gmail.com>
2020-01-10 11:44:34 +0000

    Ali - 10/01/20 - 2

--- Models/UserData/TransitUserLogin.php
+++ Models/UserData/TransitUserLogin.php
@@ -22,17 +22,18 @@ class TransitUserLogin extends TransitUserError
 
     public function verifyRole($email)
     {
-        if(substr($email, 0, 1 ) === "A")
+        if (substr($email, 0, 1) === "A")
 
-         // Prepare a select statement
-         $sql = "SELECT userName FROM students WHERE userName = :userName";
+            // Prepare a select statement
+            $sql = "SELECT userName FROM students WHERE userName = :userName";
 
-         if ($stmt = $this->_dbHandle->prepare($sql)) {
-             // Bind variables to the prepared statement as parameters
-             $stmt->bindParam(":userName", $param_userName, PDO::PARAM_STR);
- 
-             // Set parameters
-             $param_userName = trim($userName);
+        if ($stmt = $this->_dbHandle->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":userName", $param_userName, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_userName = trim($email);
+        }
     }
 
     // Authenticate user login
