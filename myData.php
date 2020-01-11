0dcbe8a9d59f36505870be6d8f246845200255d9
stevenjhume <steven.hume32@btinternet.com>
2020-01-11 22:09:09 +0000

    myData update -Ste

--- myData.php
+++ myData.php
@@ -1,8 +1,23 @@
 <?php
 
+require_once('Models/UserData/UserData.php');
+
 session_start();
 
 $view = new stdClass();
 $view->pageTitle = 'My Data';
+
+//Sets a user
+$view->user = null;
+$view->userName = null;
+
+//Checks if a user is set
+if(isset($_SESSION['user']))
+{
+    //The user that is currently logged in
+    $view->user = $_SESSION['user'];
+
+    var_dump($view->user);
+}
 
 require_once('Views/myData.phtml');
