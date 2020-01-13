10c22099119d7b8a49d705ad9f20b3ea596a9e4c
codingchristoff <50449421+codingchristoff@users.noreply.github.com>
2020-01-13 11:03:28 +0000

    update to code

--- Models/RubricSet/rubricHandler.php
+++ Models/RubricSet/rubricHandler.php
@@ -4,66 +4,73 @@ require_once('');
 
 class RubricHandler
 {
-    protected $dbHandle, $dbInstance;
+    protected $dbHandle;
+    protected $dbInstance;
     public function __construct()
     {
         $this->dbInstance = Database::getInstance();
         $this->dbHandle = $this->dbInstance->getdbConnection();
-
     }
 
-    public function createRubric()
-    {
-
-
-    }
     public function checkRubricName($rubricName)
     {
         //checks if value exists in database
         $sql = "SELECT * FROM rubrics WHERE rubricName = :rubricName";
     
-            if ($stmt = $this->_dbHandle->prepare($sql))
-            {
-                // Bind variables to the prepared statement as parameters
-                $stmt->bindParam(":rubricName", $param_rubricName, PDO::PARAM_STR);
+        if ($stmt = $this->_dbHandle->prepare($sql)) {
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":rubricName", $param_rubricName, PDO::PARAM_STR);
     
-                // Set parameters
-                $param_rubricName = trim($rubricName);
+            // Set parameters
+            $param_rubricName = trim($rubricName);
     
-                // Attempt to execute the prepared statement
-                if ($stmt->execute())
-                {
-                    if ($stmt->rowCount() == 1)
-                    {
-                        $row = $stmt->fetch();
-                        return new Rubric($row);                        
-                    }
-                    else
-                    {
-                        return false;
-                    }
+            // Attempt to execute the prepared statement
+            if ($stmt->execute()) {
+                if ($stmt->rowCount() == 1) {
+                    $row = $stmt->fetch();
+                    return new Rubric($row);
+                } else {
+                    return false;
                 }
-                else
-                {
-                    return "An error has occurred, please try again later.";
-                }
+            } else {
+                return "An error has occurred, please try again later.";
             }
-            //Close statement
-            unset($stmt);
-            //Close connection
-            unset($pdo);
         }
-           public function createRubric($rubricName)
-           {
-               
-
-
-           }
-        //if it does then 
-
-
+        //Close statement
+        unset($stmt);
+        //Close connection
+        unset($pdo);
     }
 
+    public function createRubric($rubricName)
+    {
+        $verify = checkRubricName($rubricName);
 
+        //checks if the value returned is a Rubric object.
+        if (is_a($verify, 'Rubric')) {
+            // Prepare an insert statement
+            $sql = "INSERT INTO rubric (rubricName) VALUES (:rubricName)";
 
+            if ($stmt = $this->_dbHandle->prepare($sql)) {
+                // Bind variables to the prepared statement as parameters
+                $stmt->bindParam(":rubricName", $param_rubricName, PDO::PARAM_STR);
+
+                // Set parameters
+                $param_rubricName = trim($rubricName);
+
+                // Attempt to execute the prepared statement
+                if ($stmt->execute()) {
+                    return "Name added to DB.";
+                } else {
+                    return "Something went wrong. Please try again later.";
+                }
+            }
+        } else {
+        }
+        
+        // Close statement
+        unset($stmt);
+        // Close connection
+        unset($pdo);
+    }
 }
