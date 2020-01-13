814dee9da784239353be80a347643281d654e57e
kirky1 <alex.kirkman53@gmail.com>
2020-01-13 13:06:47 +0000

    Add user page more material - Alex

--- adminAddUserPage.php
+++ adminAddUserPage.php
@@ -1,16 +1,35 @@
 <?php
 require_once('Models/UserData/UserData.php');
+require_once('Models/UserData/UserDataSet.php');
 
 session_start();
 
 $view = new stdClass();
 $view->pageTitle = 'Add User';
 
-
+//Checks if a USER has logged in
 if(isset($_SESSION['user']))
 {
+    //Checks if an ADMIN is logged in
+    if (gettype($_SESSION['user']) === 'AdminData'){
 
-    $view->user = $_SESSION['user'];
+        //Checks if admin clicked 'Create User button'
+        if(isset($_POST['submit'])){
+
+            //Initiating a connection to database
+            $dataSet = new UserDataSet();
+
+            //Checks if admin entered a username that DOES NOT already exists
+            if ($dataSet->fetchUser($_POST['userName']) != null){
+
+            }
+
+
+
+        }
+    }
+
+
 
 }
 
