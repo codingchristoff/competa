261a7a9fdfcad21eaa4f4695e655e671a2e6c3d1
codingchristoff <50449421+codingchristoff@users.noreply.github.com>
2020-01-13 10:31:09 +0000

    check function added in prep for adding to DB

--- Models/RubricSet/rubricHandler.php
+++ Models/RubricSet/rubricHandler.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once('');
 
 class RubricHandler
 {
@@ -16,8 +17,50 @@ class RubricHandler
 
 
     }
-    public function createRubricFromName($rubricName)
+    public function checkRubricName($rubricName)
     {
+        //checks if value exists in database
+        $sql = "SELECT * FROM rubrics WHERE rubricName = :rubricName";
+    
+            if ($stmt = $this->_dbHandle->prepare($sql))
+            {
+                // Bind variables to the prepared statement as parameters
+                $stmt->bindParam(":rubricName", $param_rubricName, PDO::PARAM_STR);
+    
+                // Set parameters
+                $param_rubricName = trim($rubricName);
+    
+                // Attempt to execute the prepared statement
+                if ($stmt->execute())
+                {
+                    if ($stmt->rowCount() == 1)
+                    {
+                        $row = $stmt->fetch();
+                        return new Rubric($row);                        
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return "An error has occurred, please try again later.";
+                }
+            }
+            //Close statement
+            unset($stmt);
+            //Close connection
+            unset($pdo);
+        }
+           public function createRubric($rubricName)
+           {
+               
+
+
+           }
+        //if it does then 
+
 
     }
 
