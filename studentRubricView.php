d6d670a855e34cfe6665bc5122db1965768b319c
skysnip3z <jcu8710@gmail.com>
2020-01-13 15:21:30 +0000

    Ali - 13/01/20 - 2

--- studentRubricView.php
+++ studentRubricView.php
@@ -6,7 +6,7 @@ session_start();
 $view = new stdClass();
 $view->user = null;
 $view->rubricsInfo = null;
-$view->newRubricsInfo = null;
+$view->newRubricsInfo = array();
 
 
 if(isset($_SESSION['username']))
@@ -18,7 +18,30 @@ if(isset($_SESSION['rubricsInfo']))
 {
     $view->rubricsInfo = array_slice($_SESSION['rubricsInfo'], 0, -1);
 
-    $view->rubricsInfoNew = array($view->rubricsInfo);
+    $dimension = 0;
+    $rubric = $_SESSION['rubric_count'];
+    $category = $_SESSION['category_count'];
+    $criteria = $_SESSION['criteria_count'];
+
+    $indexDivide = count($view->rubricsInfo)/$rubric;
+    $lengthCount = 0;
+    $offsetCount = $rubric-1;
+
+
+    while($lengthCount < 3)
+    {
+        $offset = $indexDivide * $offsetCount;
+        $length = $indexDivide * $lengthCount;
+
+        $view->newRubricsInfo[$dimension][$offsetCount] = array_slice($view->rubricsInfo, $offset, -$length);
+
+        var_dump($offset);
+        var_dump($length);
+        $offsetCount--;
+        $lengthCount++;
+    }
+
+
 }
 
 $view->pageTitle = 'Rubric View';
