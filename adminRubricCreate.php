254b10a789c596c7fdd316280ffb377be9942f92
stevenjhume <steven.hume32@btinternet.com>
2020-01-20 11:37:43 +0000

    Fixed dropdown for nav and added redirect for adminRubricCreate -Ste

--- adminRubricCreate.php
+++ adminRubricCreate.php
@@ -7,18 +7,29 @@ $view = new stdClass();
 $view->user = null;
 
 
-if(isset($_SESSION['username']))
+if(isset($_SESSION['user']))
 {
-    $view->user = $_SESSION['username'];
+    $view->user = $_SESSION['user'];
+
+    if ($_SESSION['user']->getRoleID() === '1')
+    {
+        if(isset($_POST['submit']))
+        {
+            //$_SESSION['rubric_count'] = $_POST['rubric_count'];
+            $_SESSION['category_count'] = $_POST['category_count'];
+            $_SESSION['criteria_count'] = $_POST['criteria_count'];
+
+            header("Location: adminRubricFill.php");
+        }
+    }
+    else
+    {
+        header('Location: home.php');
+    }
 }
-
-if(isset($_POST['submit']))
+else
 {
-//    $_SESSION['rubric_count'] = $_POST['rubric_count'];
-    $_SESSION['category_count'] = $_POST['category_count'];
-    $_SESSION['criteria_count'] = $_POST['criteria_count'];
-
-    header("Location: adminRubricFill.php");
+    header('Location: index.php');
 }
 
 $view->pageTitle = 'Rubric Create';
